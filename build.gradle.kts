ab6ea89d809f269514dc351c7dddd5d4c4e6a062
DasBabyPixel <43410952+DasBabyPixel@users.noreply.github.com>
2024-02-18 14:08:33 +0100

    0.2.34

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 description = "A gradle plugin to easily work with stracciatella modules"
 group = "net.stracciatella.gradle.plugin"
-version = "0.2.33"
+version = "0.2.34"
 
 dependencies {
     implementation(libs.gson)
